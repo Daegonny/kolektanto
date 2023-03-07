608d68adb00266dc8547223173e22bf27fb444ca
Daegonny <igorchagasm@hotmail.com>
2023-03-07 11:25:17 -0300

    feat: add get item by id function

--- lib/kolektanto/items/items.ex
+++ lib/kolektanto/items/items.ex
@@ -3,14 +3,23 @@ defmodule Kolektanto.Items do
   alias Kolektanto.Repo
   alias Kolektanto.Tags
 
+  @spec create(map()) :: {:ok, Item.t()} | {:error, Ecto.Changeset.t()}
   def create(item) do
     tags = item[:tags] || item["tags"] || []
 
     %Item{}
     |> Item.changeset(item)
     |> Ecto.Changeset.put_assoc(:tags, Tags.upsert_all(tags))
-    |> IO.inspect()
     |> Repo.insert()
-    |> IO.inspect()
   end
+
+  @spec get(binary()) :: {:ok, Kolektanto.Item.t()} | {:error, :not_found}
+  def get(item_id) do
+    case Repo.get(Item, item_id) do
+      %Item{} = item -> {:ok, preload_tags(item)}
+      nil -> {:error, :not_found}
+    end
+  end
+
+  defp preload_tags(item), do: Repo.preload(item, :tags)
 end
--- test/kolektanto/items_test.exs
+++ test/kolektanto/items_test.exs
@@ -1,16 +1,14 @@
 defmodule Kolektanto.ItemsTest do
   use Kolektanto.DataCase, async: true
   alias Kolektanto.{Item, Items}
-  alias Kolektanto.Repo
 
-  describe "items/1" do
+  describe "create/1" do
     test "creates an item" do
       params = %{name: name = "item 1", tags: tags = ["large", "green", "expansive"]}
       assert {:ok, %Item{} = item} = Items.create(params)
-      item_with_tags = Repo.preload(item, :tags)
 
-      assert item_with_tags.name == name
-      item_tag_names = Enum.map(item_with_tags.tags, & &1.name)
+      assert item.name == name
+      item_tag_names = Enum.map(item.tags, & &1.name)
       assert Enum.all?(tags, &(&1 in item_tag_names))
     end
 
@@ -20,15 +18,12 @@ defmodule Kolektanto.ItemsTest do
       params_2 = %{name: "item 2", tags: tags}
 
       assert {:ok, %Item{} = item_1} = Items.create(params_1)
-      item_1_with_tags = Repo.preload(item_1, :tags)
-
       assert {:ok, %Item{} = item_2} = Items.create(params_2)
-      item_2_with_tags = Repo.preload(item_2, :tags)
 
-      item_1_tags = MapSet.new(item_1.tags)
-      item_2_tags = MapSet.new(item_2.tags)
+      tags_1 = MapSet.new(item_1.tags)
+      tags_2 = MapSet.new(item_2.tags)
 
-      assert MapSet.equal?(item_1_tags, item_2_tags)
+      assert MapSet.equal?(tags_1, tags_2)
     end
 
     test "returns errors when name is not given" do
@@ -36,6 +31,20 @@ defmodule Kolektanto.ItemsTest do
 
       refute changeset.valid?
       assert [name: {"can't be blank", [validation: :required]}] = changeset.errors
+    end
+  end
+
+  describe "get/1" do
+    test "returns item with its tags given an id" do
+      item = insert(:item)
+      assert {:ok, inserted_item} = Items.get(item.id)
+
+      assert item == inserted_item
+    end
+
+    test "returns error tuple when item is not found" do
+      non_existing_id = Faker.UUID.v4()
+      assert {:error, :not_found} = Items.get(non_existing_id)
     end
   end
 end
