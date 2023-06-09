891ad68f2125f0969b2bc2d6e03dbd40553ca75e
Daegonny <igorchagasm@hotmail.com>
2023-06-09 17:04:18 -0300

    feat: set API error format

--- test/kolektanto_web/views/error_view_test.exs
+++ test/kolektanto_web/views/error_view_test.exs
@@ -1,15 +1,16 @@
 defmodule KolektantoWeb.ErrorViewTest do
+  @moduledoc false
   use KolektantoWeb.ConnCase, async: true
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(KolektantoWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(KolektantoWeb.ErrorView, "404.json", []) == %{errors: [%{message: "Not Found"}]}
   end
 
   test "renders 500.json" do
     assert render(KolektantoWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+             %{errors: [%{message: "Internal Server Error"}]}
   end
 end
