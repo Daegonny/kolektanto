2df3e014ac91cabc6ad065e8874f421e19bc65c4
Daegonny <igorchagasm@hotmail.com>
2024-05-25 12:30:20 -0300

    chore: add database seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,24 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Kolektanto.Items.Item
+alias Kolektanto.Tags.Tag
+alias Kolektanto.Repo
+alias Faker
+
+tag_names = ~w(summer winter autumn spring red green blue yellow day night)
+
+tags =
+  Enum.map(tag_names, fn tag_name ->
+    Repo.insert!(%Tag{name: tag_name})
+  end)
+
+for _ <- 1..20 do
+  random_quantity = Faker.random_between(0, 4)
+  tags = Enum.take_random(tags, random_quantity)
+
+  Repo.insert!(%Item{
+    name: Faker.Pokemon.En.name(),
+    tags: tags
+  })
+end
